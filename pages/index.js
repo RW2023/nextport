b1e1ce17cc00d6f2e8a055853d8482a8f07c1d76
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-22 15:58:10 -0400

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1 className="text-4xl font-bold text-center">Ryan Wilson</h1>
-    </main>
+    <>
+      <Navbar />
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <h1 className="text-4xl font-bold text-center">Ryan Wilson</h1>
+      </main>
+    </>
   )
 }
