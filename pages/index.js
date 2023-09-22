880f9e3b93657aa9ec4cf0056fb9c43d9562b074
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-22 16:39:24 -0400

    move navbar to hero and render hero in index

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Hero from '@/components/Hero/Hero';
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-      >
-        <h1 className="text-4xl font-bold text-center">Ryan Wilson</h1>
-      </main>
-    </>
-  )
+  
+      <Hero />
+    
+  );
 }
