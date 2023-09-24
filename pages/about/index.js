8ba4ba37fa4e4c2d9cb64fc2adb53a9c32650c18
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-24 03:25:47 -0400

    update about me page with styling

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,9 +3,33 @@ import 'daisyui/dist/full.css'; // Import Daisy UI styles
 
 function About() {
     return (
-        <div>
-            <h1 className="text-2xl font-bold">About</h1>
-            <p className="text-lg">This is the about page</p>
+        <div className=" min-h-screen py-12">
+            <div className="container mx-auto text-center">
+                <h1 className="text-4xl font-bold mb-8">About Me</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                    <div>
+                        <img
+                            src="https://via.placeholder.com/300"
+                            alt="Your Image"
+                            className="rounded-full w-60 mx-auto"
+                        />
+                    </div>
+                    <div className="text-left space-y-4">
+                        <p className="text-xl">
+                            Hello! I'm [Your Name], a passionate [Your Profession] based in [Your Location].
+                        </p>
+                        <p className="text-xl">
+                            With over [X years] of experience in [Field], I specialize in [Specialties].
+                        </p>
+                        <p className="text-xl">
+                            When I'm not [What you do professionally], you can find me [Hobbies/Interests].
+                        </p>
+                        <button className="btn btn-primary">
+                            Download Resume
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
