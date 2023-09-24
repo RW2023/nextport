bc82c8df6d7effb08ebcf269706c94d93e456c72
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-24 18:24:50 -0400

    Restore Navbar some items got left behind during animation TS

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -40,7 +40,13 @@ const Navbar = () => {
             <i className="fas fa-bars"></i>
           </label>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            {/* ... menu items ... */}
+            <li><a><i className="fas fa-home"></i> Homepage</a></li>
+            <li><a><i className="fas fa-briefcase"></i> Portfolio</a></li>
+            <li><a><i className="fas fa-info-circle"></i> About</a></li>
+            <li><a><i className="fas fa-laptop-code"></i> Technologies</a></li>
+            <li><a><i className="fas fa-blog"></i> Blog</a></li>
+            <li><a><i className="fas fa-envelope"></i> Contact</a></li>
+            <li><a><i className="fas fa-code-branch"></i> GitHub Repo</a></li>
           </ul>
         </div>
       </div>
@@ -50,7 +56,15 @@ const Navbar = () => {
         </a>
       </div>
       <div className="navbar-end">
-        {/* ... other buttons ... */}
+        <button className="btn btn-ghost btn-circle">
+          <i className="fas fa-search"></i>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <i className="fas fa-bell"></i>
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
       </div>
     </div>
   );
