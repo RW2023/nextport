0f4726b3a45d3a272ff2d48d63fb38e7c497ff52
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-23 21:04:24 -0400

    make navbar dark

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -5,20 +5,19 @@ const Hero = () => {
   return (
     <>
       <Navbar />
-      <div data-theme="dark" className="navbar bg-base-100">
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content text-center">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">
-                <i className="fa-solid fa-code"></i> Hello <i className="fa-solid fa-code"></i>
-              </h1>
-              <p className="py-6">
-                <i className="fa-solid fa-globe"></i> Welcome to my Portfolio Website <i className="fa-solid fa-globe"></i>
-              </p>
-              <button className="btn btn-primary">
-                <i className="fa-solid fa-terminal"></i> Come In! <i className="fas fa-door-open"></i>
-              </button>
-            </div>
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/Z2bQfwF/background.png)'}}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">
+              <i className="fa-solid fa-code"></i> Hello <i className="fa-solid fa-code"></i>
+            </h1>
+            <p className="py-6">
+              <i className="fa-solid fa-globe"></i> Welcome to my Portfolio Website <i className="fa-solid fa-globe"></i>
+            </p>
+            <button className="btn btn-primary">
+              <i className="fa-solid fa-terminal"></i> Come In! <i className="fas fa-door-open"></i>
+            </button>
           </div>
         </div>
       </div>
