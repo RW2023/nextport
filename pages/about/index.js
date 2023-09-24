fe9c014294371c184944321bae63f682a03a0784
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-23 21:05:10 -0400

    create about me section

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'daisyui/dist/full.css'; // Import Daisy UI styles
+
+function About() {
+    return (
+        <div className="p-4"> 
+            <h1 className="text-3xl font-bold text-center">About Me</h1>
+        </div>
+    );
+}
+
+export default About;
