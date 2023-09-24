3bc675ec0b1e07fc1c4b708b4198e7d2d74e27f9
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-24 12:04:02 -0400

    update hero

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <>
       <Navbar />
       <div className="hero min-h-screen" style={{backgroundImage: 'url(/img/freepikbg2.jpg)'}}>
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-opacity-80"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">
               <i className="fa-solid fa-code"></i> Hello <i className="fa-solid fa-code"></i>
             </h1>
-            <p className="py-6">
+            <p className="py-6 text-headline">
               <i className="fa-solid fa-globe"></i> Welcome to my Portfolio Website <i className="fa-solid fa-globe"></i>
             </p>
             <button className="btn btn-primary">
