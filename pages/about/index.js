cb5ef89666dfc64accb327c515a13d9d4514ef37
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-23 21:13:17 -0400

    create about me component

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,8 +3,19 @@ import 'daisyui/dist/full.css'; // Import Daisy UI styles
 
 function About() {
     return (
-        <div className="p-4"> 
+        <div className="p-4">
             <h1 className="text-3xl font-bold text-center">About Me</h1>
+            <div className="grid grid-cols-3 gap-4"> {/* Using CSS grid with 3 columns and some gap */}
+                <div className="bg-blue-100 p-4 rounded-lg"> {/* Adding some styling to the grid items */}
+                    <p>grid 1</p> {/* Changed <p1> to <p> */}
+                </div>
+                <div className="bg-blue-100 p-4 rounded-lg">
+                    <p>grid 2</p>
+                </div>
+                <div className="bg-blue-100 p-4 rounded-lg">
+                    <p>grid 3</p>
+                </div>
+            </div>
         </div>
     );
 }
