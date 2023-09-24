efb8cff5662c446f39bfa0651cd8e2369a60e68f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-24 09:50:16 -0400

    make navbar sticky

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div data-theme="dark">
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 sticky top-0" data-theme="dark">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
@@ -31,7 +30,7 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-    </div>
+    
   );
 };
 
