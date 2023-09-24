b3e2d254da8a45d0c7e735ba133ca7c7bdb2664f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-24 03:09:33 -0400

    apply dark them to project

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en" >
+    <Html lang="en" data-theme="dark"  >
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
