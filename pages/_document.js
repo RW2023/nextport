5888f9708d0d082cfee49afc698997261fd9aac0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-25 03:37:55 -0400

    Optimized Head section for better readability and cleanliness

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,16 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="dark"  >
       <Head>
+        <title>Ryan Wilson</title>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Ryan Wilson's Portfolio" />
         <meta name="author" content="Ryan Wilson" />
-        <meta name="keywords" content="Ryan Wilson, Portfolio, Web Developer, 
-        Software Developer, Front End Developer, Back End Developer, 
-        Full Stack Developer, MERN Stack, React, Next.js, Junior Developer, 
-        London, Ontario, Canada, Web Development, Coding, Programming,
-         Software Engineering, Responsive Design, UI/UX, JavaScript, HTML, CSS, 
-         Node.js, Express, MongoDB, Git, GitHub, Version Control" />
+        <meta name="keywords" content="Ryan Wilson, Portfolio, Web Developer, Software Developer, Front End Developer, Back End Developer, Full Stack Developer, React, Next.js, Junior Developer, London, Ontario, Canada, Web Development, Coding, Programming, Software Engineering, Responsive Design, UI/UX, JavaScript, HTML, CSS, Node.js, Express, MongoDB, Git, GitHub, Version Control" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Ryan Wilson</title>
-
       </Head>
+
       <body>
         <Main />
         <NextScript />
