62b10b8ba311cdaacebf4ec672cf7894de65683f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-25 15:34:39 -0400

    add navbar and footer to project page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,30 +1,35 @@
 // pages/projects/index.js
 import React from 'react';
 import Image from 'next/image';
+import Navbar from '@/components/Navbar/Navbar';
+import Footer from '@/components/Footer/Footer';
 
 const Projects = () => {
     return (
-        <div className="container mx-auto p-4">
-            <div className="card bordered max-w-md mx-auto">
-                <figure>
-                    <Image
-                        src="/img/placeholder.jpg"
-                        alt="Placeholder"
-                        width={500}
-                        height={300}
-                    />
-                </figure>
-                <div className="card-body">
-                    <h1 className="card-title text-2xl">Projects Coming Soon!</h1>
-                    <p>We're working hard to list all the cool projects here. Stay tuned!</p>
-                    <div className="card-actions">
-                        <button className="btn btn-primary">Stay Updated</button>
+        <div> 
+            <Navbar />
+            <div className="container mx-auto p-4">
+                <div className="card bordered max-w-md mx-auto">
+                    <figure>
+                        <Image
+                            src="/img/placeholder.jpg"
+                            alt="Placeholder"
+                            width={500}
+                            height={300}
+                        />
+                    </figure>
+                    <div className="card-body">
+                        <h1 className="card-title text-2xl">Projects Coming Soon!</h1>
+                        <p>We're working hard to list all the cool projects here. Stay tuned!</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Stay Updated</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </div> 
     );
 };
 
 export default Projects;
-
