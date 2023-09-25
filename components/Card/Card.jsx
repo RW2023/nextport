0c67dbe34dfcacc5e971d0a933a083854930a961
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-25 06:25:12 -0400

    u[date image tag

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -4,7 +4,7 @@ function Card() {
     return (
         <div>
             <div className="card card-side bg-slate-400-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+                <figure><Image src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
                 <div className="card-body">
                     <h2 className="card-title">New movie is released!</h2>
                     <p>Click the button to watch on JetFlix app.</p>
