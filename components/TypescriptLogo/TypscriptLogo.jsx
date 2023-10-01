f4afd67330291abc9f3044f8029cbb10b06fa3b6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-01 12:49:50 -0400

    update alt tag and viewbox

--- components/TypescriptLogo/TypscriptLogo.jsx
+++ components/TypescriptLogo/TypscriptLogo.jsx
@@ -9,6 +9,8 @@ function TypscriptLogo() {
                 src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAACJ0lEQVR4nO2Xy2sTQRzH998YS1VqLz0WwYJeRLS0eCiCiBQVb4Jn2ajtwUsfBqS1SEVsYyooHsRzKra1FInSqpDnZtM8mhbdYBrqpmn6SOYru7Rpx6TYHNKd4Hzhe9idx85n5jczv5UkISEhIS5FZA94siQAZLECECFUiawOGSI2sWz9rBNxjMrWzzwRF5nMh6VKZfWAiQCQrZ91IkJI3n9G5uJZVCr7uFZsf+y+F7Z3S3gf0JFIb2BlLW9a07fwdSGLl5+Xcdoe4hOguScIRcv9s/41R4xPgI+hTEnZVoEinc0z7y4/i1QPoG0ojCvPo0VfHYkyHw/8yDHlhlv6FZzqU5h6BQrcebuEo/e8Zr8NXT7ceBHHVEjHxSfzh3cP1N/1MgNzR1fL1uscjTH1JhV93z7rbB7+AK47WIDgz5y5oS0/Rg8KcLI3CErZ+J+LZ9H6OFwbAIbfzKZLNjGlgMv/G+cGVP4Bmh748X1xrQTCUL5A4XSncPyvsOIKwHBjtw9j7mXzFCqnL7HV4snEJcCOLwyGMaHoZSH6XRr/AGTbl55GsJjeZPownmsGgMgenLGHmD6M8Dpi4wzg7CMVt18nigMje2zc1HtlJHfcrUDHcMQsV5PreDiumRfb+UHVTNy+Jdj8alrN8AtwEN10xmsTgFJg4EOSaccNwIluH269WoDjUwoz8xnEUhtmGv0rs/sj0z5UmlZUHaDalv47ACEhISHpMPQHFt1nSGvrWBcAAAAASUVORK5CYII="
                 width={48}
                 height={48}
+                viewBox="0 0 48 48"
+                alt='Typescript'
             />
         </React.Fragment>
     );
