34b6275149655d4d24f55a26c14c22d69c6c47f8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-01 20:39:38 -0400

    set application theme color

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
         <meta name="description" content="Ryan Wilson's Portfolio" />
         <meta name="author" content="Ryan Wilson" />
         <meta name="keywords" content="Ryan Wilson, Portfolio, Web Developer, Software Developer, Front End Developer, Back End Developer, Full Stack Developer, React, Next.js, Junior Developer, London, Ontario, Canada, Web Development, Coding, Programming, Software Engineering, Responsive Design, UI/UX, JavaScript, HTML, CSS, Node.js, Express, MongoDB, Git, GitHub, Version Control" />
