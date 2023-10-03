bbb09e4f6289a445501d57084d532de37c830091
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-03 03:17:34 -0400

    update welcome page

--- pages/welcome/index.js
+++ pages/welcome/index.js
@@ -10,7 +10,7 @@ function Welcome() {
       <Navbar />
       <div
         className="bg-gray-900 min-h-screen py-12 px-4 sm:px-6 lg:px-8 text-white"
-        data-theme="dark"
+        data-theme="business"
       >
         <div className="max-w-7xl mx-auto">
           <h1 className="text-4xl font-extrabold text-center mb-4">
@@ -21,7 +21,7 @@ function Welcome() {
             the categories below:
           </p>
           <div className="mt-12 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-            <div className="card bordered hover:bg-primary hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
+            <div className="card bordered hover:bg-stroke hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
               <figure>
                 {' '}
                 <Image
@@ -51,7 +51,7 @@ function Welcome() {
               <div className="card-body bg-black">
                 <h2 className="card-title text-headline">About Me</h2>
                 <p className="text-paragraph">
-                  Learn more about me and my skills.
+                  Learn more about me and how i got here.
                 </p>
               </div>
             </div>
@@ -73,9 +73,9 @@ function Welcome() {
             <div className="card bordered hover:bg-primary hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
               <figure>{/* Optionally insert an image here */}</figure>
               <div className="card-body bg-black">
-                <h2 className="card-title">Something Else</h2>
+                <h2 className="card-title">Other Interests</h2>
                 <p className="text-paragraph">
-                  Explore additional content here.
+                  Explore my other interests and hobbies.
                 </p>
               </div>
             </div>
