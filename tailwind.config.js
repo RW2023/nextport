a394bb630c21204a32992c10560c72c9c5a8f59d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-04 18:04:12 -0400

    add waving hand to hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,28 +6,28 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   daisyui: {
-    themes: true
+    themes: true,
   },
   theme: {
     extend: {
       colors: {
-        'overlay': 'rgba(184, 193, 236, 0.5)',
-        'background': '#232946',
-        'button': '#ff8906',
-        'headline': '#fffffe',
-        'paragraph': '#b8c1ec',
-        'buttonText': '#232946',
-        'stroke': '#121629',
-        'main': '#b8c1ec',
-        'highlight': '#eebbc3',
-        'secondary': '#fffffe',
-        'tertiary': '#eebbc3',
+        overlay: 'rgba(184, 193, 236, 0.5)',
+        background: '#232946',
+        button: '#ff8906',
+        headline: '#fffffe',
+        paragraph: '#b8c1ec',
+        buttonText: '#232946',
+        stroke: '#121629',
+        main: '#b8c1ec',
+        highlight: '#eebbc3',
+        secondary: '#fffffe',
+        tertiary: '#eebbc3',
+        nubian: '#9E6240',
       },
       fontFamily: {
-        'sans': ['Poppins', 'sans-serif'],
+        sans: ['Poppins', 'sans-serif'],
       },
-      
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 };
