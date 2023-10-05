c26448f1d641ba47e86de0b4107886faf610c78a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-05 04:38:45 -0400

    move projects from component into mongo dbConnect create a dbConnect file to connect to mongo db create an api  to handle api calls to project db at this point it is working connected to local mongo will now attempt to connect to mongo atlas

---
+++ pages/api/projects.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
+
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    await client.connect();
+    const db = client.db('portfolio'); 
+    const collection = db.collection('projects');
+
+    const projects = await collection.find({}).toArray();
+
+    return res.status(200).json(projects);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Unable to fetch projects' });
+  } finally {
+    await client.close();
+  }
+}
