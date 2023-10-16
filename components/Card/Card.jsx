fc33c694e3cb58ed8ec0703b68459dca57d921ed
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-16 01:49:48 -0400

    Restored original grid styling for project cards

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
@@ -13,7 +12,7 @@ import NodeLogo from '../NodeLogo/NodeLogo';
 import Tailwind from '../Tailwind/Tailwind';
 import Mongo from '../Mongo/Mongo';
 import TypescriptLogo from '../TypescriptLogo/TypscriptLogo';
-import Loading from '../Loading/Loading'; // Adjust the path to where your Loading.js is
+import Loading from '../Loading/Loading';
 
 const getTechLogo = (tech) => {
   switch (tech) {
@@ -37,8 +36,6 @@ const getTechLogo = (tech) => {
       return <Mongo key="mongo" />;
     case 'TypeScript':
       return <TypescriptLogo key="TypeScript" />;
-    case 'Mongo':
-      return <Mongo key="mongo" />;
     default:
       return null;
   }
@@ -47,16 +44,12 @@ const getTechLogo = (tech) => {
 function Card() {
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       const response = await axios.get('/api/projects');
-      setLoading(true);
-      const response = await axios.get('/api/projects');
       setProjects(response.data);
-      setLoading(false);
       setLoading(false);
     };
 
@@ -69,68 +62,59 @@ function Card() {
         <span className="fa fa-briefcase mr-3"></span>Projects
       </h1>
       {loading ? (
-        <span className="loading loading-bars loading-lg">
-          <p className='text-'>Loading Projects...</p>
-        </span>
-      ) : (
-        {loading ? (
         <Loading text="Projects" />
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-3 gap-4 justify-center">
-            {projects.map((project, index) => (
-              <div
-                className="card bordered max-w-md w-full mx-auto my-4"
-                data-theme="luxury"
-                key={index}
-              >
-                <figure>
-                  <Image
-                    src={project.image}
-                    width={500}
-                    height={500}
-                    alt={project.title}
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title text-2xl">{project.title}</h2>
-                  <p>{project.description}</p>
-                  <div className="flex flex-wrap space-x-2">
-                    <h3 className="text-xl">Tech Stack:</h3>
-                    {project.stack &&
-                      project.stack.map((tech, i) => (
-                        <div key={i} className="tech-logo">
-                          {getTechLogo(tech)}
-                        </div>
-                      ))}
-                  </div>
-                  <div className="card-actions">
-                    <Link
-                   
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {projects.map((project, index) => (
+            <div
+              className="card bordered max-w-md w-full mx-auto my-4"
+              data-theme="luxury"
+              key={index}
+            >
+              <figure>
+                <Image
+                  src={project.image}
+                  width={500}
+                  height={500}
+                  alt={project.title}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-2xl">{project.title}</h2>
+                <p>{project.description}</p>
+                <div className="flex flex-wrap space-x-2">
+                  <h3 className="text-xl">Tech Stack:</h3>
+                  {project.stack &&
+                    project.stack.map((tech, i) => (
+                      <div key={i} className="tech-logo">
+                        {getTechLogo(tech)}
+                      </div>
+                    ))}
+                </div>
+                <div className="card-actions">
+                  <Link
                     href={project.github}
-                   
                     aria-label="View project on GitHub"
-                  
                   >
-                      <button className="btn btn-primary bg-button border-stroke text-stroke">
-                        View on GitHub
+                    <button className="btn btn-primary bg-button border-stroke text-stroke">
+                      View on GitHub
+                    </button>
+                  </Link>
+                  {project.liveVersion && (
+                    <Link
+                      href={project.liveVersion}
+                      aria-label="View live version of project"
+                    >
+                      <button className="btn btn-secondary bg-stroke border-headline hover:bg-primary hover:border-stroke hover:text-stroke">
+                        Live Version
                       </button>
                     </Link>
-                    {project.liveVersion && (
-                      <Link
-                        href={project.liveVersion}
-                        aria-label="View live version of project"
-                      >
-                        <button className="btn btn-secondary bg-stroke border-headline hover:bg-primary hover:border-stroke hover:text-stroke">
-                          Live Version
-                        </button>
-                      </Link>
-                    )}
-                  </div>
+                  )}
                 </div>
               </div>
-            ))}
-          </div>
-      )}
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
