37e4803741f403e78c9659d0b947039175210281
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-15 23:22:22 -0400

    linkify welcom page components change data theme to black

--- pages/welcome/index.js
+++ pages/welcome/index.js
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
 import Image from 'next/image';
 import 'daisyui/dist/full.css';
+import Link from 'next/link';
 
 function Welcome() {
   return (
@@ -10,7 +11,7 @@ function Welcome() {
       <Navbar />
       <div
         className="flex flex-wrap justify-center items-center min-h-screen dark:bg-gray-900 border-stroke"
-        data-theme="dark"
+        data-theme="black"
       >
         <div className="max-w-7xl mx-auto">
           <h1 className="text-4xl font-extrabold text-center mb-4">
@@ -22,15 +23,16 @@ function Welcome() {
           </p>
           <div className="mt-12 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
             <div className="card bordered hover:bg-stroke hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
-              <figure>
-                {' '}
-                <Image
-                  src="/img/placeholder.jpg"
-                  alt="Placeholder"
-                  width={500}
-                  height={300}
-                />
-              </figure>
+              <Link href={'/projects/'}>
+                <figure>
+                  <Image
+                    src="/img/placeholder.jpg"
+                    alt="Placeholder"
+                    width={500}
+                    height={300}
+                  />
+                </figure>
+              </Link>
               <div className="card-body bg-black">
                 <h2 className="card-title text-headline">Projects</h2>
                 <p className="text-paragraph">
@@ -39,32 +41,35 @@ function Welcome() {
               </div>
             </div>
             <div className="card bordered hover:bg-primary hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
-              <figure>
-                <Image
-                  src="/img/me.jpg"
-                  alt="A picture of Ryan Wilson"
-                  className="rounded-full w-60 mx-auto"
-                  width={240} // Set the width
-                  height={300} // Set the height
-                />
-              </figure>
+              <Link href={'/about/'}>
+                <figure>
+                  <Image
+                    src="/img/me.jpg"
+                    alt="A picture of Ryan Wilson"
+                    className="rounded-full w-60 mx-auto"
+                    width={240}
+                    height={300}
+                  />
+                </figure>
+              </Link>
               <div className="card-body bg-black">
                 <h2 className="card-title text-headline">About Me</h2>
                 <p className="text-paragraph">
-                  Learn more about me and how i got here.
+                  Learn more about me and how I got here.
                 </p>
               </div>
             </div>
             <div className="card bordered hover:bg-primary hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
-              <figure>
-                {' '}
-                <Image
-                  src="/img/connect.jpg"
-                  alt="Placeholder"
-                  width={500}
-                  height={300}
-                />
-              </figure>
+              <Link href={'/contact/'}>
+                <figure>
+                  <Image
+                    src="/img/connect.jpg"
+                    alt="Placeholder"
+                    width={500}
+                    height={300}
+                  />
+                </figure>
+              </Link>
               <div className="card-body bg-black">
                 <h2 className="card-title">Contact</h2>
                 <p className="text-paragraph">Get in touch with me.</p>
