3d0a34defb9f59a2aaf889dda838ec58bc9d0ef7
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-19 05:50:44 -0400

    refactor and update about page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,41 +1,27 @@
 import React from 'react';
 import 'daisyui/dist/full.css';
-import Navbar from '@/components/Navbar/Navbar';
-import Footer from '@/components/Footer/Footer';
 import GitHub from '@/components/github/github';
 
 function About() {
   return (
-    <>
-      <Navbar />
-      <div
-        className="bg-gray-900 min-h-screen py-12 px-4 sm:px-6 lg:px-8 sm:mt-7 text-white pt-20"
-        data-theme="black"
-      >
-        <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-bold mb-8">
-            <span className="fa-solid fa-circle-info mr-3"></span>About Me
-          </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-            <div className="card bordered hover:bg-stroke hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105">
-              <GitHub />
-              {/* <figure>
-                <Image
-                  src="/img/me.jpg"
-                  alt="A picture of Ryan Wilson"
-                  className="rounded-full w-60 mx-auto border-4 border-primary"
-                  width={240} // Set the width
-                  height={240} // Set the height
-                />
-              </figure> */}
-            </div>
-            <div className="card bordered p-6 hover:bg-stroke hover:shadow-xl transition ease-in-out duration-200 transform hover:scale-105 text-left space-y-4">
-              <p className="text-l">
-                Hello! My name is Ryan, a passionate Junior Full Stack Developer
-                based in London, Ontario, Canada.
-              </p>
-              <h2 className="text-xl font-bold">My Story</h2>
-              <h3 className="text-l font-bold">How I got here</h3>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 mt-20">
+      <div className="flex flex-col justify-center text-center">
+        <GitHub />
+      </div>
+      <div>
+        <h1 className="text-headline text-bold text-center text-3xl md:text-4xl">
+          <span className="fa-solid fa-circle-info mr-3 mt-10 text-center text-4xl"></span>
+          About Me
+        </h1>
+        <div className="flex flex-col items-center justify-center text-center p-4">
+          <div className="bg-main text-stroke card bordered p-5 shadow-lg max-w-2xl mx-auto">
+            <p className="text-i mb-4">
+              Hello! My name is Ryan, a passionate Junior Full Stack Developer
+              based in London, Ontario, Canada.
+            </p>
+            <h2 className="text-xl font-bold mb-2">My Story</h2>
+            <h3 className="text-l font-bold mb-2">How I got here</h3>
+            <div className="space-y-4">
               <p className="text-l">
                 I have always had a passion for technology and computers. From a
                 young age, I was fascinated with machines and how they worked. I
@@ -64,28 +50,29 @@ function About() {
                 ever be a programmer. I felt like coding was just not for me.
               </p>
               <p className="text-l">
-                My latest brush with coding, in my mid 40's is the most fulfilling. The pandemic was raging
-                and my brain was not happy. I still could not shake this desire
-                to learn how to code
+                My latest brush with coding, in my mid 40's is the most
+                fulfilling. The pandemic was raging and my brain was not happy.
+                I still could not shake this desire to learn how to code
               </p>
+            </div>
+            <div className="mt-4 text-l">
               I decided i would give it the final do, not try. It never ends.
               which is one of the things i like the most about it. there is
               always another door to open. A deeper level of understanding that
               can be reached. Here I am. Courses and videos later. I am a junior
               full stack developer. I am still learning which may be painfully
               obvious. I am here though 👋🏾.
-              <p className="text-l">
-                I chose the MERN stack because I wanted to learn the most
-                popular stack. I also wanted to learn a stack that would allow
-                me to build a full application. I am glad I chose it. Next JS is
-                a very nice Dev tool 😁
-              </p>
             </div>
+            <p className="text-l mt-4">
+              I chose the MERN stack because I wanted to learn the most popular
+              stack. I also wanted to learn a stack that would allow me to build
+              a full application. I am glad I chose it. Next JS is a very nice
+              Dev tool 😁
+            </p>
           </div>
         </div>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 }
 
