a5e88820a918b773313b11834176f6fba4ae7996
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-18 21:38:40 -0400

    change project card layout to fit better on tablet devices

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -64,7 +64,7 @@ function Card() {
       {loading ? (
         <Loading text="Projects" />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {projects.map((project, index) => (
             <div
               className="card bordered max-w-md w-full mx-auto my-4"
