f1f8d9a2fb7ef777bf4cb81f3a4397dc810986a2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-14 06:55:57 -0500

    import proper nav from another project where I got it the way i wanted it in the beginning

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,54 +1,93 @@
-'use client';
-import React from 'react';
-import Link from 'next/link';
+// src/Components/UI/Navbar.tsx
+
+'use client'; 
+
 import { useState } from 'react';
-import HamburgerMenu from '../Hamburger/Hamburger';
+import Link from 'next/link';
 
-
-function Navbar() {
-  const [navbar, setNavbar] = useState(false);
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full bg-black fixed top-0 left-0 right-0 z-10">
-      <div className="justify-between px-4 mx-auto md:items-center md:flex md:px-8">
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
-          {/* LOGO */}
-          <Link href="/">
-            <div className="cursor-pointer text-2xl">RW</div>
-          </Link>
-          {/* HAMBURGER BUTTON FOR MOBILE */}
-          <div className="md:hidden">
-            <button
-              className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-              onClick={() => setNavbar(!navbar)}
-            >
-              {navbar ? <HamburgerMenu /> : <HamburgerMenu />}
-            </button>
-          </div>
-        </div>
-        <div
-          className={`flex-none md:flex ${
-            navbar ? 'block' : 'hidden md:block'
-          }`}
+    <nav className="sticky-top-0 z-50 flex items-center justify-between flex-wrap bg-base-500 p-6 font-sans navbar mt-3">
+      <div className="flex items-center flex-shrink-0 font-bold  text-2xl mr-6">
+        <Link href="/">
+          <span className="font-semibold text-xl tracking-tight cursor-pointer">
+            Ryan Wilson{' '}
+          </span>
+        </Link>
+      </div>
+      <div className="block lg:hidden">
+        <button
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-xl inline-flex items-center justify-center p-2 rounded-md hover:text-buttonText hover:bg-button focus:outline-none focus:ring-2 focus:ring-inset focus:ring-base-500"
         >
-          <ul className="menu menu-horizontal px-1 text-2xl">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
+          {isOpen ? (
+            // Close icon when the menu is open
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            // Hamburger icon when the menu is closed
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+      <div
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } w-full lg:flex lg:items-center lg:w-auto lg:justify-end`}
+      >
+        <div className="text-sm lg:flex-grow">
+          <Link href="/">
+            <span className="block mt-4 lg:inline-block lg:mt-0 text-strokeLight text-xl hover:text-strokeDark  mr-4 cursor-pointer">
+              Home
+            </span>
+          </Link>
+          <Link href="/projects">
+            <span className="block mt-4 lg:inline-block lg:mt-0 text-strokeLight text-xl hover:text-strokeDark  mr-4 cursor-pointer">
+              Projects
+            </span>
+          </Link>
+          <Link href="/about">
+            <span className="block mt-4 lg:inline-block lg:mt-0 text-strokeLight text-xl hover:text-buttonText mr-4 cursor-pointer">
+              About
+            </span>
+          </Link>
+          <Link href="/contact">
+            <span className="block mt-4 lg:inline-block lg:mt-0 text-strokeLight text-xl hover:text-buttonText cursor-pointer">
+              Contact
+            </span>
+          </Link>
         </div>
       </div>
-    </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
